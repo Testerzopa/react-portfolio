38ea08358b17bafdd7bd6d8cea8208c5badf9d6d
Testerzopa <testerzopa111@gmail.com>
2026-05-07 13:32:54 +0700

    fix problem #2

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './navbar.css'
 
 function Navbar(){
@@ -17,26 +17,4 @@ function Navbar(){
             </div>
     )
 }
-
-=======
-import './navbar.css'
-
-function Navbar(){
-    return(
-            <div className="container">
-                <div className="logo">
-                    <h1>Teekawin</h1>
-                </div>
-                <div className="content">
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-    )
-}
-
->>>>>>> d40e8c5c95f28d784d479359fc7e6b372bbd6475
 export default Navbar
